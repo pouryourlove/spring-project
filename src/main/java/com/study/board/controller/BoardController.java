6fe08b2145b05cc20991ed28cd910ab3b76e5a6b
pouryourlove <lqu134@naver.com>
2021-11-25 09:38:57 +0900

    Initial commit

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BoardController {
+    @GetMapping("/board/write")
+    public String boardWriteForm(){
+
+        return "boardwrite";
+
+    }
+}
---
+++ src/main/java/com/study/board/domain/Board.java
@@ -0,0 +1,25 @@
+package com.study.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+public class Board extends Timestamped{
+
+    @Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+
+}
+
