0a168e50bf097f41c1a485b80127bfc6ebd6f1e2
pouryourlove <lqu134@naver.com>
2021-11-28 22:15:04 +0900

    데이터 전달 postmapping으로 완료

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -10,6 +11,17 @@ public class BoardController {
     public String boardWriteForm(){
 
         return "boardwrite";
+    }
+
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content){
+
+        System.out.println("제목 : " + title);
+        System.out.println("내용 : " + content);
+
+
+        return "";
 
     }
 }
